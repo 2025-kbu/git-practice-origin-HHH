44917d97de5b69be1206053aff5c8b0a94102c56
kodulgi <cjylove0107@gmail.com>
2025-04-25 11:28:17 +0900

    Add option to show day of week

--- scripts/date_tool.sh
+++ scripts/date_tool.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+# 날짜 계산 스크립트 (요일 포함)
 
-# 날짜 계산 스크립트
 if [ $# -eq 0 ]; then
   echo "Usage: $0 [+N | -N]"
   exit 1
 fi
 
-date --date="$1" +"%Y-%m-%d"
+date --date="$1" +"%Y-%m-%d (%A)"
